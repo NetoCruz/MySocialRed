1b6c56c160f237ad1b91f4f61f26c4cfbc72ccfb
netocruz <galaxy.net0710@gmail.com>
2022-09-19 13:50:24 -0500

    Page contacts added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { Route,Routes } from 'react-router-dom';
+import Header from './Components/Header'
+import Gallery from './Components/Gallery';
+import Profile from './Components/Profile';
+import Data from './Data'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <Routes>
+       
+         <Route  path='/' element={<Gallery/>}/>
+         <Route  path='/profile/:id' element={<Profile data={Data}/>}/>
+       </Routes>
+      // <div>
+      // <Header/>
+      // <Gallery/>
+      // <Profile/></div>
+
   );
 }
 
