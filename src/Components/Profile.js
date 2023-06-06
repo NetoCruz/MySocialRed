3f34a703c1b3f68839f1141db68228e12bf3e67b
Neto Cruz <galaxy.net0710@gmail.com>
2023-06-06 15:49:51 -0600

    profile update

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,9 +1,6 @@
 import React from "react";
 import "./Profile.css";
 import work from "../images/work.svg"
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-
-
 import { useParams } from "react-router-dom";
 
 export default function Profile({data}){
@@ -11,113 +8,86 @@ export default function Profile({data}){
     return(
       
     
-
-         <div>
+ 
+         
             
-            <div >
+            <>
           {
-            data
-              .filter(list => list.id === parseInt(id)  )
-              .map((list) => (
-                <div  key={ list.id }>
-                <div className="profile--contact">
-                   
-                    <img className="profile--photo" src={`/${list.img}`} alt="photoprofile"/> 
-                    <div className="profile--data">
-                      <h2> {list.name}</h2>
-                      <p>{list.ocupation}</p>
-                        <div className="work--icon"> 
-                            <img src={work} alt="workicon" height={"30px"}/> 
-                        <p>{list.work}</p>
+              data
+                .filter(list => list.id === parseInt(id)  )
+                .map((list) => (
+                  <div  key={ list.id }>
+                    <div className="profile--contact">
+                      
+                        <img className="profile--photo" src={`/${list.img}`} alt="photoprofile"/> 
+                        <div className="profile--data">
+                          <h2> {list.name}</h2>
+                          <p>{list.ocupation}</p>
+                            <div className="work--icon"> 
+                                <img src={work} alt="workicon" height={"30px"}/> 
+                            <p>{list.work}</p>
+                            </div>
+                          
+                          <button>Contact</button>
                         </div>
-                       
-                       <button>Contact</button>
+                        <div className="profile--price">
+                            <span>${list.price}/hora</span>
+                        </div>
+                        <div className="profile--social">
+                            <p>Disponibilidad: Completa</p>
+                            <p>Edad: {list.age}</p>
+                            <p>Residencia: {list.location}</p>
+                        </div>
                     </div>
-                    <div className="profile--price">
-                        <span>${list.price}/hora</span>
-                    </div>
-                    <div className="profile--social">
-                        <p>Disponibilidad: Completa</p>
-                        <p>Edad: {list.age}</p>
-                        <p>Residencia: {list.location}</p>
-                    </div>
-                </div>
-                <div className="profile--likes">
-                {list.likes.map((item)=>(
-                    <p className="likes--text">{item}</p>
-                   ))}
-                </div>
-
-                <Tabs className="Tabs">
-       <TabList>
-         <Tab>Descripción</Tab>
-         <Tab>Servicios</Tab>
-         <Tab>Galleria</Tab>
-        
-       </TabList>
-<TabPanel>
-<div className="profile--description">
-                    <p>Descripción</p>
+                    {/* <div className="profile--likes">
+                    {list.likes.map((item)=>(
+                        <p className="likes--text">{item}</p>
+                      ))}
+                    </div> */}
                     
-                    <div className="text">{list.description}</div>
+                    <section class="detail">
+                      <div> 
+                      <div class="fotografia">
+                      <p>fotos</p>
+                      <div class="galleria">
+                        <img src="../../public/bris.jpg" width="100px" height="100px" alt=""/>
+                        <img src="../../public/bris.jpg" width="100px" height="100px" alt=""/>
+                        <img src="../../public/bris.jpg" width="100px" height="100px" alt=""/>
+                        <img src="../../public/bris.jpg" width="100px" height="100px" alt=""/>
+                      </div>
+                        </div>
+                        <div class="interes">
+                        <p>intereses</p>
+                        <div class="lista">
+                        {list.likes.map((item)=>(
+                        <p className="likes--text">{item}</p>
+                      ))}
+                        </div>
+                            
+                            </div>
+                        </div>
+                        <div class="about">
+                            <p>Acerca de mi</p>
+                            <p>Mujer discreta y amable. A veces es necesario salir de la rutina y <br/>
+                                relajarse un poco. Soy divorciada y tengo varias responsabilidades.</p>
+                        </div>
+                        <div class="prices">
+                          <p>Tarifas</p> 
+                          <p>1hr  $1200.00</p>
+                          <p>1hr  $1200.00</p>
+                          <p>1hr  $1200.00</p>
+                        </div>
+                      </section>
                     
-                    </div> 
-       </TabPanel>
-       <TabPanel>
-       <div className="profile--services">
-                   <p>Servicios</p>
-                   <div>
-                     <p>Aquí van los servicios</p>
-                   </div>
-                    </div> 
-       </TabPanel>
-       <TabPanel>
-       <div className="gallery">
+                    </div>
                   
-                  {
-                    list.photos.map((item)=>(
-                      <img src={`/${item}`} alt="photogallery"/>
-                    ))
-                  }
-
-                </div>
-       </TabPanel>
-     </Tabs>
-   
-                
-                {/* <div className="profile--descriptions">
-                   <div className="profile--services">
-                   <p>Servicios</p>
-                    </div> 
-                   <div className="profile--description">
-                    <p>Descripción</p>
-                    
-                    <div className="text">{list.description}</div>
-                    
-                    </div> 
-                   <div className="profile--related">
-                   <p>Perfiles Relacionados</p>
+                ))
               
-                    </div>  
-                </div>
-
-                <div className="gallery">
-                  <p>Galería</p>
-                  {
-                    list.photos.map((item)=>(
-                      <img src={`/${item}`} alt="photogallery"/>
-                    ))
-                  }
-
-                </div> */}
-                
-                  
-                </div>
-              ))}
-        </div>
+              }
+        </>
 
            
-         </div>
+         
        
     )
 }
