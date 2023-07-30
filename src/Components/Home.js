905b731f511721a951db1272415b2e81669443a3
Neto Cruz <galaxy.net0710@gmail.com>
2023-07-30 11:00:45 -0600

    Modal added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,21 @@
-import React from "react";
+
 import VideoPlayer from "./VideoPlayer"
+import Modal from "./Modal";
+
+import React, { useState } from "react";
+import   "./Home.css";
 
 
 
 export default function Home(){
-
+const [isOpen, setIsOpen] = useState(false);
     return(
         <>
         <h1>My social Red</h1>
+        <button className="primaryBtn" onClick={() => setIsOpen(true)}>
+        Open Modal
+      </button>
+      {isOpen && <Modal setIsOpen={setIsOpen} />}
          <VideoPlayer/>
         </>
     )
